c39520be0fea05d5c238c68c06934a499b5405d8
Tasneem Hasanat <tasneemhasanat97@gmail.com>
2022-09-18 20:30:05 -0400

    setup tab bar
    set up initial tab bar view controller

---
+++ Flixy/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixy
+//
+//  Created by Tasneem Hasanat on 9/18/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
